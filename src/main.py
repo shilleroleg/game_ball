8d5da8767bc99c28ecdbd24e642bdbdd37985a30
shilleroleg <shilleroleg@gmail.com>
2020-05-03 01:07:34 +0700

    add Ball class TODO: create ball in loop

--- src/main.py
+++ src/main.py
@@ -1,9 +1,49 @@
 import tkinter as tk
+from random import randint
+
+WIDTH = 50
+HEIGHT = 50
+
+
+class Ball:
+    def __init__(self):
+        self.R = 20
+        self.x = randint(1.1*self.R, WIDTH-1.1*self.R)
+        self.y = self.R
+        self.dx, self.dy = (+3, +3)
+
+        self.ball_id = canvas.create_oval(self.x - self.R, self.y - self.R,
+                                          self.x + self.R, self.y + self.R,
+                                          fill='green')
+
+    def move(self):
+        self.x = self.x
+        self.y += self.dy
+
+    def show(self):
+        canvas.move(self.ball_id, self.dx, self.y)
+
+    def get_coord(self):
+        return self.x, self.y
+
+
+def tick():
+    print(balls.get_coord()[1])
+    balls.move()
+    balls.show()
+    if balls.get_coord()[1] > HEIGHT:
+        return
+
+    root.after(100, tick)
+
+
 
 
 def main():
+    global root, canvas, balls
+
     root = tk.Tk()
-    root.minsize(width=500, height=400)
+    root.minsize(width=2*WIDTH, height=2*HEIGHT)
     root.title("Ball")
 
     tk.Label(root, text="Score:", font="Times 14").grid(row=0, column=0, sticky=tk.W, pady=10, padx=10)
@@ -17,15 +57,11 @@ def main():
     canvas.grid(row=1, column=0, columnspan=4)
 
     # Мяч
-    ball = canvas.create_oval(0, 100, 40, 140, fill='green')
-
-    def motion():
-        canvas.move(ball, 1, 0)
-        if canvas.coords(ball)[2] < 500:
-            root.after(10, motion)
-
-    motion()
-
+    num_balls = 0
+    while num_balls < 2:
+        balls = Ball()
+        tick()
+        num_balls += 1
 
     root.mainloop()
 
