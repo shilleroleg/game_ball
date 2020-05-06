95b53fb04e0fe4bcb5acab582784c04357a112a9
shilleroleg <shilleroleg@gmail.com>
2020-05-06 23:42:24 +0700

    First stable version ver. 0.0.1

--- src/simple_ball.py
+++ src/simple_ball.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from random import randint, choice
 
 WIDTH = 300
-HEIGHT = 200
+HEIGHT = 400
 
 
 class Ball:
@@ -12,7 +12,6 @@ class Ball:
         self.y = self.R
         self.dx = 0 if dx is None else dx
         self.dy = 2 if dy is None else dy
-        # self.dy = dy
 
         self.color = choice(['blue', 'green', 'red', 'brown'])
 
@@ -21,16 +20,16 @@ class Ball:
                                           self.x + self.R,
                                           self.y + self.R,
                                           fill=self.color)
-        # canvas.tag_bind(self.ball_id, '<ButtonPress-1>', func=canvas.delete(self.ball_id))
+        canvas.tag_bind(self.ball_id, '<ButtonPress-1>', func=self.delete_ball)
 
     def move(self):
         canvas.move(self.ball_id, self.dx, self.dy)
 
     def get_coord(self):
         return canvas.coords(self.ball_id)
-    #
-    # def delete_ball(self, *args):
-    #     canvas.delete(self.ball_id)
+
+    def delete_ball(self, *args):
+        canvas.delete(self.ball_id)
 
 
 class Menu:
@@ -62,28 +61,53 @@ class Menu:
         self.val_score_num -= count
         self.val_score_label['text'] = str(self.val_score_num)
 
+        # Если очков меньше нуля, уменьшает жизнь на 1,
+        # и восстанавливаем начальное кол-во очков
+        if self.val_score_num < 1:
+            self.life_decrease()
+            self. score_increase(10)
+
     def life_increase(self):
-        pass
+        # Увеличивает кол-во жизней
+        self.val_life_num += 1
+        self.val_life_label['text'] = str(self.val_life_num)
 
-    def life_decrease(self):
-        pass
+    def life_decrease(self):    # TODO: Закончить игру при 0 жизней
+        # Уменьшает кол-во жизней
+        self.val_life_num -= 1
+        self.val_life_label['text'] = str(self.val_life_num)
 
 
-def create_ball():
+def create_ball(dx, dy):
+    """
+    Создаем новый объект шарик
+
+    :param dx: Шаг смещения по оси х
+    :param dy: Шаг смещения по оси у
+    :return: Объект шар
+    """
     global ball_id
-    ball_id = Ball(0, 2)
+    ball_id = Ball(dx, dy)
 
 
 def motion():
-    # canvas.move(ball_id, 0, 1)
+    # TODO: Сначала добавляем несколько шариков одновременно на экране
+    # TODO: Затем ускоряем падение шариков
     ball_id.move()
-    # print(canvas.coords(ball_id))
-    if int(ball_id.get_coord()[1]) < HEIGHT + ball_id.R:
-        root.after(15, motion)
-    else:
-        # print("Else")
+
+    # Если шарик лопнул, увеличиваем очки на 3 и перезапускаем
+    if len(ball_id.get_coord()) == 0:
+        menu.score_increase(3)
+        create_ball(0, 2)
+
+    # Если шарик НЕ вышел за границы экрана - продолжаем
+    if len(ball_id.get_coord()) > 0 \
+            and int(ball_id.get_coord()[1]) < HEIGHT + ball_id.R:
+        root.after(25, motion)
+    else:   # Иначе, уменьшаем очки на 1 и перезапускаем
         canvas.delete(ball_id)
-        create_ball()
+        menu.score_decrease(1)
+        create_ball(0, 2)
         motion()
 
 
@@ -97,7 +121,7 @@ def main():
 
     menu = Menu()
 
-    create_ball()
+    create_ball(0, 2)
     motion()
 
     root.mainloop()
